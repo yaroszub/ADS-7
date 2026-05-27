29b3704e5f2cc8d31f8a9545104da756b70ff6dc
yaroszub <yaroslav.zubkov.2020@bk.ru>
2026-05-27 19:18:23 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,48 @@
-// Copyright 2022 NNTU-CS
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+    std::srand(static_cast<unsigned int>(time(nullptr)));
 
-  while (count--)
-    train.addCar(false);
+    std::cout << "n\tfalse\t\ttrue\t\trndom\n";
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    for (int n = 2; n <= 100; n++) {
+        int stepsFalse, stepsTrue, stepsRandom;
+
+        // Все лампочки выключены
+        {
+            Locomotive train;
+            for (int i = 0; i < n; i++)
+                train.appendWagon(false);
+            train.measureLength();
+            stepsFalse = train.getSteps();
+        }
+
+        // Все лампочки включены
+        {
+            Locomotive train;
+            for (int i = 0; i < n; i++)
+                train.appendWagon(true);
+            train.measureLength();
+            stepsTrue = train.getSteps();
+        }
+
+        // Случайные состояния
+        {
+            Locomotive train;
+            for (int i = 0; i < n; i++)
+                train.appendWagon(std::rand() % 2);
+            train.measureLength();
+            stepsRandom = train.getSteps();
+        }
+
+        std::cout << n << "\t"
+                  << stepsFalse << "\t\t"
+                  << stepsTrue << "\t\t"
+                  << stepsRandom << "\n";
+    }
+
+    return 0;
 }
