ccf5e36303a2eb08707082a1ef4bd486caa4658e
yaroszub <yaroslav.zubkov.2020@bk.ru>
2026-05-29 21:03:25 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -44,31 +44,24 @@ int Train::getLength() {
     resetCounter();
     now = head;
 
-    // если лампочка выключена – включаем
     if (!now->light) now->light = true;
 
     int length = 0;
     bool finished = false;
 
     while (!finished) {
-        // идём вперёд до первого горящего вагона
         while (true) {
             moveForward();
             ++length;
             if (now->light) {
-                now->light = false;   // гасим
+                now->light = false;
                 break;
             }
         }
-
-        // возвращаемся назад на length шагов
         for (int i = 0; i < length; ++i) moveBackward();
-
-        // если лампочка погашена – мы у начала
         if (!now->light) finished = true;
-        else length = 0;   // иначе начинаем заново
+        else length = 0;
     }
-
     return length;
 }
 
