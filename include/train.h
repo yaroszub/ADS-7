e3b63ff8b8e40a3cd3462f1076ee18924d3429c5
yaroszub <yaroslav.zubkov.2020@bk.ru>
2026-05-29 21:00:02 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,23 +1,28 @@
 #ifndef TRAIN_H
 #define TRAIN_H
-class Locomotive {
+
+class Train {
 private:
-    struct Wagon {
-        bool light;   // состояние лампочки
-        Wagon* next;
-        Wagon* prev;
+    struct Car {
+        bool light;
+        Car* next;
+        Car* prev;
     };
-    int steps;        // счётчик переходов между вагонами
-    Wagon* head;      // первый вагон (точка входа)
-    Wagon* current;   // текущий вагон
-    void resetSteps() { steps = 0; }
-    void forward();   // переход вперёд с увеличением счётчика
-    void backward();  // переход назад с увеличением счётчика
+
+    int operations;   // счётчик переходов (раньше countOp)
+    Car* head;        // первый вагон
+    Car* now;         // текущий вагон (раньше current)
+
+    void resetCounter() { operations = 0; }
+    void moveForward();   // переход вперёд + счётчик
+    void moveBackward();  // переход назад + счётчик
+
 public:
-    Locomotive();
-    ~Locomotive();
-    void appendWagon(bool light);    // добавить вагон
-    int measureLength();             // вычислить длину состава
-    int getSteps() const;            // получить число переходов
+    Train();
+    ~Train();
+    void addCar(bool light);
+    int getLength();
+    int getOpCount() const;
 };
+
 #endif
